df550bbd57b996f001cb38f9d056935703c8d777
tavisca-vkumar <vkumar@tavisca.in>
2019-06-23 21:17:20 +0530

    commit by tavisca-vkumar

--- Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
@@ -13,7 +13,7 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
             Test("2*12?=247", -1);
             Console.ReadKey(true);
         }
-
+        
         private static void Test(string args, int expected)
         {
             var result = FindDigit(args).Equals(expected) ? "PASS" : "FAIL";
@@ -23,6 +23,43 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
         public static int FindDigit(string equation)
         {
             // Add your code here.
+            int star = -1, question = -1, equal = -1, bug=1, ans = -1; 
+            for(int i=0; i<equation.Length; i++)
+            {
+                if (equation[i] == '?')
+                {
+                    question = i;
+                    if (star <= 0)
+                        bug = 1;
+                    else if (equal <= 0)
+                        bug = 2;
+                    else
+                        bug = 3;
+                }
+
+                if (equation[i] == '*')
+                     star = i;
+                if (equation[i] == '=')
+                    equal = i;
+                //Console.WriteLine(equation[i]);
+            }
+            
+            for(int i=0; i<=9; i++)
+            {
+
+                if (i == 0 && (question == 0 || question == star + 1 || question == equal + 1))
+                    continue;
+                string str = equation.Substring(0, question) + Convert.ToString(i) + equation.Substring(question + 1);
+                //Console.WriteLine(str);
+                
+                if (Convert.ToInt32(str.Substring(0, star)) * Convert.ToInt32(str.Substring(star + 1, equal - star - 1)) == Convert.ToInt32(str.Substring(equal + 1)))
+                {
+                    ans = i;
+                    break;
+                }
+                //Console.WriteLine("--{0}--", i);
+            }
+            return ans;
             throw new NotImplementedException();
         }
     }
